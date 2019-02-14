59895a1f0fab954e8e18fb681145b6d5c29071de
Christian Gomez <10721401@my.uvu.edu>
2019-02-14 12:50:57 -0700

    CC

---
+++ Unity/Character Movment/Assets/Scripts/MvoeCharacter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MvoeCharacter : MonoBehaviour
+{
+    public float MoveSpeed = 5;
+    public float Gravity = -9.81f;
+    
+    private CharacterController CC;
+    private Vector3 postion;
+    
+    
+    void Start()
+    {
+        CC = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        postion.x = Input.GetAxis("Horizontal")*MoveSpeed*Time.deltaTime;
+        if (CC.isGrounded)
+        {
+            
+        }
+        
+        
+        postion.y = Gravity * Time.deltaTime;
+        
+        CC.Move(postion);
+    }
+}
