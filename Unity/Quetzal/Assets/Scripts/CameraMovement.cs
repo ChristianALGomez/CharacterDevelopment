3b91493411ec4d25df3c1e0f2ccd1e1b16ae785c
Christian Gomez <10721401@my.uvu.edu>
2019-04-30 15:05:41 -0600

    Quetzal in unity

---
+++ Unity/Quetzal/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+    public Vector2 rot;
+    private void LateUpdate()
+    {
+        if (player != null)
+        {
+            transform.position = player.transform.position + offset;
+        }
+        
+    }
+}
---
+++ Unity/Quetzal/Assets/Scripts/FloatDataScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewFloat", menuName = "CG/FloatData")]
+public class FloatDataScript : MonoBehaviour
+{
+    public float value;
+    
+    public virtual float Value
+    { 
+        get { return value;}
+    }
+}
